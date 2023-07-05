e3bed33deb9ab35809fd6ba4f9896d58a40de454
heisemberg <heissen1122@gmail.com>
2023-07-05 12:26:19 -0500

    Six commit

--- gestion.py
+++ gestion.py
@@ -10,28 +10,31 @@ if __name__ == '__main__':
         os.system('cls')
         print(f'1. Registrar Producto\n2. Mostrar Productos\n3. Registrar compra\n4. Mostrar Compras\n5. Registrar venta\n6. Mostrar ventas')
         opcion = int(input('Selecione una opcion: '))
-        if opcion == 1:
-            inventario.addProduct(products)
-        
-        elif opcion == 2:
-            os.system('cls')
-            print(products)
-            os.system('pause')
+        try:
+            if opcion == 1:
+                inventario.addProduct(products)
 
-        elif opcion == 3:
-            os.system('cls')
-            inventario.regShopping(shopping, products)
+            elif opcion == 2:
+                os.system('cls')
+                print(products)
+                os.system('pause')
 
-        elif opcion == 4:
-            os.system('cls')
-            print(shopping)
-            os.system('pause')
+            elif opcion == 3:
+                os.system('cls')
+                inventario.regShopping(shopping, products)
 
-        elif opcion == 5:
-            os.system('cls')
-            inventario.regSales(sales, products)
+            elif opcion == 4:
+                os.system('cls')
+                print(shopping)
+                os.system('pause')
 
-        elif opcion == 6:
-            os.system('cls')
-            print(sales)
-            os.system('pause')
+            elif opcion == 5:
+                os.system('cls')
+                inventario.regSales(sales, products)
+
+            elif opcion == 6:
+                os.system('cls')
+                print(sales)
+                os.system('pause')
+        except Exception:
+            isAddTeam= bool(input("Desea continuar en el programa S(Si) o Enter(No) :"))
