5175d40938e2471853a441d13146cf0367b3fdec
heisemberg <heissen1122@gmail.com>
2023-07-05 22:44:20 -0500

    Seven commit

--- gestion.py
+++ gestion.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     isAddProducts = True
     while isAddProducts:
         os.system('cls')
-        print(f'1. Registrar Producto\n2. Mostrar Productos\n3. Registrar compra\n4. Mostrar Compras\n5. Registrar venta\n6. Mostrar ventas')
+        print(f'1. Registrar Producto\n2. Mostrar Productos\n3. Eliminar Producto\n4. Registrar compra\n5. Mostrar Compras\n6. Registrar venta\n6. Mostrar ventas')
         opcion = int(input('Selecione una opcion: '))
         try:
             if opcion == 1:
@@ -16,23 +16,34 @@ if __name__ == '__main__':
 
             elif opcion == 2:
                 os.system('cls')
-                print(products)
+                clave=(products.keys())
+                print('+','-'*76,'+')
+                print("| {:^5} | {:^12} | {:^5} | {:^10} | {:^12} | {:^7} | {:^7} |".format('COD','NOMBRE','STOCK','ESTADO','PROVEEDOR','VAL.COM','VAL.VEN'))
+                
+                for i in clave:
+                    print('+','-'*76,'+')
+                    print("| {:^5} | {:^12} | {:^5} | {:^10} | {:^12} | {:^7} | {:^7} |".format(products[i]['codigo'], products[i]['nombre'],products[i]['stock'],products[i]['estado'],products[i]['proveedor'],products[i]['valorCompra'],products[i]['valorVenta']))
+                print('+','-'*76,'+')    
                 os.system('pause')
-
+            
             elif opcion == 3:
+                os.system('cls')
+                inventario.remProduct(products)
+
+            elif opcion == 4:
                 os.system('cls')
                 inventario.regShopping(shopping, products)
 
-            elif opcion == 4:
+            elif opcion == 5:
                 os.system('cls')
                 print(shopping)
                 os.system('pause')
 
-            elif opcion == 5:
+            elif opcion == 6:
                 os.system('cls')
                 inventario.regSales(sales, products)
 
-            elif opcion == 6:
+            else:
                 os.system('cls')
                 print(sales)
                 os.system('pause')
--- inventario.py
+++ inventario.py
@@ -15,8 +15,18 @@ def addProduct (productos : dict):
     } 
     productos.update(producto)
 
+def remProduct(productos):
+    producto = productos.get(input('Ingrese el codigo de producto: '),-1)
+        
+    if producto == -1:
+        print('Codigo de producto no encontrado')
+    else:
+        producto.pop(producto['cod'])
+
+
 def regShopping(compras : dict, productos : dict):
     isAddShopping = True
+    item = 1
     codCompra = input('Ingrese codigo de compra: ') 
     compra = {
         codCompra:{
@@ -29,28 +39,24 @@ def regShopping(compras : dict, productos : dict):
 
     while isAddShopping:
         producto = productos.get(input('Ingrese el codigo de producto: '),-1)
-        #print(producto)
+        
         if producto == -1:
             print('Codigo de producto no encontrado')
         else:
             cantCompra = int(input('Ingrese cantidad: '))
-            if cantCompra <= producto['stock']:
-                newStock = producto['stock']+cantCompra
-                valCompra = cantCompra * producto['valorCompra']
-                producto['stock']= newStock
-                producto.update({'cantCompra':cantCompra})
-                producto.update({'valCompra':valCompra})
-                #print(producto)
-                carritoCompras.update({producto['codigo']: producto})
-                #print(carritoCompras)
-                compra[codCompra].update({'compras':carritoCompras})
-                #print(compra)
-                isAddShopping = bool(input("Desea agregar otro Producto S(Si) Enter(No)"))
+            newStock = producto['stock']+cantCompra
+            valCompra = cantCompra * producto['valorCompra']
+            producto['stock']= newStock            
+            carritoCompras.update({item : {producto['codigo']: producto, 'cantCompra':cantCompra, 'valCompra':valCompra } } )
+            compra[codCompra].update({'compras':carritoCompras})
+            item+=1
+            isAddShopping = bool(input("Desea agregar otro Producto S(Si) Enter(No)"))
     compras.update(compra)
-    #print(compras)
+   
 
 def regSales(ventas : dict, productos : dict):
     isAddSale = True
+    item=1
     codVenta = input('Ingrese codigo de venta: ') 
     venta = {
         codVenta:{
@@ -63,7 +69,7 @@ def regSales(ventas : dict, productos : dict):
 
     while isAddSale:
         producto = productos.get(input('Ingrese el codigo de producto: '),-1)
-        #print(producto)
+        
         if producto == -1:
             print('Codigo de producto no encontrado')
         else:
@@ -72,13 +78,12 @@ def regSales(ventas : dict, productos : dict):
                 newStock = producto['stock']-cantVenta
                 valVenta = cantVenta * producto['valorVenta']
                 producto['stock']= newStock
-                producto.update({'cantVenta':cantVenta})
-                producto.update({'valVenta':valVenta})
-                #print(producto)
-                carritoVentas.update({producto['codigo']: producto})
-                #print(carritoCompras)
+                carritoVentas.update({item:{producto['codigo']: producto, 'cantVenta':cantVenta,'valVenta':valVenta}})
                 venta[codVenta].update({'ventas':carritoVentas})
-                #print(compra)
+                item+=1
+                isAddSale = bool(input("Desea agregar otro Producto S(Si) Enter(No)"))
+            else:
+                print('No hay stock suficiente')
                 isAddSale = bool(input("Desea agregar otro Producto S(Si) Enter(No)"))
     ventas.update(venta)
     
